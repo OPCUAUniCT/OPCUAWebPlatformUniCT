7bf1585528b73a6f7ae3b5cc26915f474e5e20fb
Marco Salafia <marco.salafia@gmail.com>
2018-02-08 11:47:02 +0100

    Added NuGet Packages for OPC UA stack

--- OPCUA_Web_Platform/Controllers/ValuesController.cs
+++ OPCUA_Web_Platform/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Opc.Ua;
 
 namespace WebPlatform.Controllers
 {
@@ -13,6 +14,7 @@ namespace WebPlatform.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
+            IEncoder encoder;
             return new string[] { "value1", "value2" };
         }
 
