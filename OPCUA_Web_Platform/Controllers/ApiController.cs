1554397d26759c9341b817965cb7f3e8662b10fb
Marco Salafia <marco.salafia@gmail.com>
2018-02-17 12:42:44 +0100

    Completed Skeleton Controllers for the API

--- OPCUA_Web_Platform/Controllers/ApiController.cs
+++ OPCUA_Web_Platform/Controllers/ApiController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebPlatform.Models.DataSet;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,23 +12,42 @@ namespace WebPlatform.Controllers
     [Route("[controller]")]
     public class ApiController : Controller
     {
-        [HttpGet("data-sources")]
-        public IActionResult GetDataSources()
+        [HttpGet("data-sets")]
+        public IActionResult GetDataSets()
         {
             return Ok("Ti ritorno gli url dei Data Set");
         }
 
-        [HttpGet("data-sources/{ds_id:int}/nodes")]
-        public IActionResult GetDataSources(int ds_id)
+        //[HttpGet("data-sources/{ds_id:int}/nodes")]
+        //public IActionResult GetDataSources(int ds_id)
+        //{
+        //    //return Ok($"Ti ritorno l'entry point al Data Set {ds_id}");
+        //    return GetDataSources(ds_id, "0-85");
+        //}
+
+        [HttpGet("data-sets/{ds_id:int}/nodes/{node_id:regex(^\\d+-\\S+$)?}")]
+        public IActionResult GetNode(int ds_id, string node_id = "0-85")
         {
-            //return Ok($"Ti ritorno l'entry point al Data Set {ds_id}");
-            return GetDataSources(ds_id, "0-85");
+            return Ok($"Ti ritorno il nodo {node_id} del data set {ds_id}");
         }
 
-        [HttpGet("data-sources/{ds_id:int}/nodes/{node_id:regex(^\\d+-\\S+$)}")]
-        public IActionResult GetDataSources(int ds_id, string node_id)
+        [HttpPut("data-sets/{ds_id:int}/nodes/{node_id:regex(^\\d+-\\S+$)}")]
+        public IActionResult PutNode(int ds_id, string node_id, [FromForm] VariableState state)
         {
-            return Ok($"Ti ritorno il nodo {node_id} del datat set {ds_id}");
+            if (!state.isValid) { return BadRequest("Insert a valid state for a Variable Node."); }
+            return Ok($"Scrivo {state.Value} sul nodo {node_id} del data set {ds_id}");
+        }
+
+        [HttpPost("data-sets/{ds_id:int}/monitor")]
+        public IActionResult Monitor(int ds_id)
+        {
+            return Ok("Monitoro");
+        }
+
+        [HttpPost("data-sets/{ds_id:int}/stop-monitor")]
+        public IActionResult StopMonitor(int ds_id)
+        {
+            return Ok($"Smonitoro");
         }
     }
 }
---
+++ OPCUA_Web_Platform/Models/DataSet/NodeState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebPlatform.Models.DataSet
+{
+    public class VariableState
+    {
+        public string Value { get; set; }
+        public bool isValid
+        {
+            get { return Value != null; }
+        }
+    }
+}
