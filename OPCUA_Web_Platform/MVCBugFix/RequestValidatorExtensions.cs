f8d5c72d50fb06d737a47b54bca18fa3ce4f2d76
Marco Stefano Scroppo <marcostefano.scroppo@outlook.com>
2018-02-27 10:31:01 +0100

    Fixato problema MVC che generava una exception quando una richiesta con content type MULTIPART aveva body vuoto

---
+++ OPCUA_Web_Platform/MVCBugFix/RequestValidatorExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebPlatform.MVCBugFix
+{
+    public static class RequestValidatorExtensions
+    {
+        // Extensions method to simplify RequestValidatorMiddleware usage 
+        public static IApplicationBuilder UseRequestValidator(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<RequestValidatorMiddleware>();
+        }
+    }
+}
---
+++ OPCUA_Web_Platform/MVCBugFix/RequestValidatorMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace WebPlatform.MVCBugFix
+{
+    public class RequestValidatorMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestValidatorMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            // Check if request body is empty and it's a multipart request
+            if (context.Request.ContentLength == 0
+                && context.Request.ContentType != null
+                && context.Request.ContentType.ToUpper().StartsWith("MULTIPART/"))
+            {
+                // Set 400 response with a message
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                await context.Response.WriteAsync("Multipart request body must not be empty.");
+            }
+            else
+            {
+                // All other requests continue the way down the pipeline
+                await _next(context);
+            }
+        }
+    }
+}
